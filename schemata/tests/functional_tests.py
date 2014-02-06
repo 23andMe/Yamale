b0b90774c0643f80547f40aa518d137e6d02330f
blopker <lopkerk@gmail.com>
2014-02-06 13:58:41 -0800

    simpler tests

--- schemata/tests/functional_tests.py
+++ schemata/tests/functional_tests.py
@@ -53,15 +53,6 @@ def test_flat_make_schema():
     assert isinstance(types['schema']['string'], val.String)
 
 
-def test_flat_validate():
-    assert sch.validate(types['schema'], types['good'])
-
-
-@raises(ValueError)
-def test_bad_validate():
-    sch.validate(types['schema'], types['bad'])
-
-
 def test_nested_schema():
     nested_schema = nested['schema']
     assert isinstance(nested_schema['string'], val.String)
@@ -69,8 +60,18 @@ def test_nested_schema():
     assert isinstance(nested_schema['list.0'], val.String)
 
 
-def test_nested_validate():
-    assert sch.validate(nested['schema'], nested['good'])
+def test_good():
+    for data_map in test_data:
+        yield good_gen, data_map
+
+
+def good_gen(data_map):
+    sch.validate(data_map['schema'], data_map['good'])
+
+
+@raises(ValueError)
+def test_bad_validate():
+    sch.validate(types['schema'], types['bad'])
 
 
 @raises(ValueError)
@@ -78,26 +79,14 @@ def test_bad_nested():
     sch.validate(nested['schema'], nested['bad'])
 
 
-def test_custom():
-    assert sch.validate(custom['schema'], custom['good'])
-
-
 @raises(ValueError)
 def test_bad_custom():
     assert sch.validate(custom['schema'], custom['bad'])
 
 
-def test_lists():
-    assert sch.validate(lists['schema'], lists['good'])
-
-
 @raises(ValueError)
 def test_bad_lists():
     assert sch.validate(lists['schema'], lists['bad'])
-
-
-def test_keywords():
-    assert sch.validate(keywords['schema'], keywords['good'])
 
 
 @raises(ValueError)
