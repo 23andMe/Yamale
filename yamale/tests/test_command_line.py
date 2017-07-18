1efbb8c46739587610ee0052af0c34c7eea454a9
Corentin Roussel <corentin.roussel@cea.fr>
2017-07-18 15:16:32 +0200

    Incorrect folder for schema in two command line tests.

--- yamale/tests/test_command_line.py
+++ yamale/tests/test_command_line.py
@@ -6,13 +6,13 @@ def test_bad_yaml():
     with pytest.raises(ValueError):
         command_line._router(
             'yamale/tests/command_line_fixtures/yamls/bad.yaml',
-            'schema.yaml', 1)
+            '../schema.yaml', 1)
 
 
 def test_good_yaml():
     command_line._router(
         'yamale/tests/command_line_fixtures/yamls/good.yaml',
-        'schema.yaml', 1)
+        '../schema.yaml', 1)
 
 
 def test_bad_dir():
