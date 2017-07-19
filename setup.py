0cdf06850c7b501dd995dd0def7a3931e903705c
Bo Lopker <blopker@23andme.com>
2017-07-19 13:11:01 -0700

    Bump version

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ elif PY3:
 
 setup(
     name='yamale',
-    version='1.6.3',
+    version='1.6.4',
     url='https://github.com/23andMe/Yamale',
     author='Bo Lopker',
     author_email='blopker@23andme.com',
