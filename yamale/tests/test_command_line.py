b35b96dfe9ac8d296d8677a3631179c52ab89d33
Bo Lopker <blopker@users.noreply.github.com>
2017-07-25 13:17:10 -0700

    Possibility to choose which parser to use for loading files (#36)
    * Possibility to choose which parser to use for loading files
    
    * Updated tests
    
    * Update README, better errors, more tests

--- yamale/tests/test_command_line.py
+++ yamale/tests/test_command_line.py
@@ -1,50 +1,59 @@
 import os
+
+import pytest
+
 from .. import command_line
 
 dir_path = os.path.dirname(os.path.realpath(__file__))
 
+parsers = ['pyyaml', 'PyYAML', 'ruamel']
 
-def test_bad_yaml():
+
+@pytest.mark.parametrize('parser', parsers)
+def test_bad_yaml(parser):
     try:
         command_line._router(
             'yamale/tests/command_line_fixtures/yamls/bad.yaml',
-            'schema.yaml', 1)
+            'schema.yaml', 1, parser)
     except ValueError as e:
-        assert 'map.bad: \'12.5\' is not a str.' in str(e)
+        assert 'Validation failed!' in str(e)
         return
     assert False
 
 
-def test_good_yaml():
+@pytest.mark.parametrize('parser', parsers)
+def test_good_yaml(parser):
     command_line._router(
         'yamale/tests/command_line_fixtures/yamls/good.yaml',
-        'schema.yaml', 1)
+        'schema.yaml', 1, parser)
 
 
-def test_good_relative_yaml():
+@pytest.mark.parametrize('parser', parsers)
+def test_good_relative_yaml(parser):
     command_line._router(
         'yamale/tests/command_line_fixtures/yamls/good.yaml',
-        '../schema_dir/external.yaml', 1)
+        '../schema_dir/external.yaml', 1, parser)
 
 
-def test_external_glob_schema():
+@pytest.mark.parametrize('parser', parsers)
+def test_external_glob_schema(parser):
     command_line._router(
         'yamale/tests/command_line_fixtures/yamls/good.yaml',
-        os.path.join(dir_path, 'command_line_fixtures/schema_dir/ex*.yaml'), 1)
+        os.path.join(dir_path, 'command_line_fixtures/schema_dir/ex*.yaml'), 1, parser)
 
 
 def test_external_schema():
     command_line._router(
         'yamale/tests/command_line_fixtures/yamls/good.yaml',
-        os.path.join(dir_path, 'command_line_fixtures/schema_dir/external.yaml'), 1)
+        os.path.join(dir_path, 'command_line_fixtures/schema_dir/external.yaml'), 1, 'PyYAML')
 
 
 def test_bad_dir():
     try:
         command_line._router(
             'yamale/tests/command_line_fixtures/yamls',
-            'schema.yaml', 4)
+            'schema.yaml', 4, 'PyYAML')
     except ValueError as e:
-        assert 'map.bad: \'12.5\' is not a str.' in str(e)
+        assert 'Validation failed!' in str(e)
         return
     assert False
