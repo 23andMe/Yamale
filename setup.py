1804b4a994937b29f3280b2ea11afdcbd0c50268
Chris Mildebrandt <chrism@23andme.com>
2020-06-26 10:05:02 -0700

    Release version 2.2.0

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ elif PY3:
 
 setup(
     name='yamale',
-    version='2.1.0',
+    version='2.2.0',
     url='https://github.com/23andMe/Yamale',
     author='Bo Lopker',
     author_email='blopker@23andme.com',
