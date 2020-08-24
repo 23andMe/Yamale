cefe7a9fa8a2a636ca96c06c9d3b7214258fb891
Chris Mildebrandt <chrism@23andme.com>
2020-08-24 15:29:18 -0700

    Release 3.0.3

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ license = open('LICENSE', encoding='utf-8').read()
 
 setup(
     name='yamale',
-    version='3.0.2',
+    version='3.0.3',
     url='https://github.com/23andMe/Yamale',
     author='Bo Lopker',
     author_email='blopker@23andme.com',
