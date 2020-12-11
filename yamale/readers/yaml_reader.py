2a7051dfc1ece16f2db0af7c51220870ddaa9cf6
zbremmer <bremmezm@gmail.com>
2020-12-11 09:08:22 -0500

    add custom yaml loader

--- yamale/readers/yaml_reader.py
+++ yamale/readers/yaml_reader.py
@@ -1,14 +1,36 @@
 from __future__ import absolute_import
+import yaml
 
+class NoDatesSafeLoader(yaml.SafeLoader):
+    @classmethod
+    def remove_implicit_resolver(cls, tag_to_remove):
+        """
+        Remove implicit resolvers for a particular tag
 
-def _pyyaml(file_name):
-    import yaml
+        Takes care not to modify resolvers in super classes.
+
+        We want to load datetimes as strings, not dates, so that we 
+        can test the format constraint. 
+        """
+        if not 'yaml_implicit_resolvers' in cls.__dict__:
+            cls.yaml_implicit_resolvers = cls.yaml_implicit_resolvers.copy()
+
+        for first_letter, mappings in cls.yaml_implicit_resolvers.items():
+            cls.yaml_implicit_resolvers[first_letter] = [(tag, regexp) 
+                                                         for tag, regexp in mappings
+                                                         if tag != tag_to_remove]
+
+NoDatesSafeLoader.remove_implicit_resolver('tag:yaml.org,2002:timestamp')
+
+def _pyyaml(f):
     try:
-        Loader = yaml.CSafeLoader
-    except AttributeError:  # System does not have libyaml
-        Loader = yaml.SafeLoader
-    with open(file_name) as f:
-        return list(yaml.load_all(f, Loader=Loader))
+        Loader = NoDatesSafeLoader
+    except: 
+        try: 
+            Loader = yaml.CSafeLoader
+        except AttributeError:  # System does not have libyaml
+            Loader = yaml.SafeLoader
+    return list(yaml.load_all(f, Loader=NoDatesSafeLoader))
 
 
 def _ruamel(file_name):
