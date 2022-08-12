4056bba459884112f25549cb960547d85722f6d9
Jonas Hensel <111120040+jhe-iqbis@users.noreply.github.com>
2022-08-12 16:30:11 +0200

    Created test for the new `-i` command line argument.

--- yamale/tests/test_command_line.py
+++ yamale/tests/test_command_line.py
@@ -33,21 +33,28 @@ def test_good_yaml(parser):
     command_line._router(
         'yamale/tests/command_line_fixtures/yamls/good.yaml',
         'schema.yaml', 1, parser)
-    
+
 
 @pytest.mark.parametrize('parser', parsers)
 def test_good_relative_yaml(parser):
     command_line._router(
         'yamale/tests/command_line_fixtures/yamls/good.yaml',
         '../schema_dir/external.yaml', 1, parser)
-    
+
 
 @pytest.mark.parametrize('parser', parsers)
 def test_external_glob_schema(parser):
     command_line._router(
         'yamale/tests/command_line_fixtures/yamls/good.yaml',
         os.path.join(dir_path, 'command_line_fixtures/schema_dir/ex*.yaml'), 1, parser)
-    
+
+
+@pytest.mark.parametrize('parser', parsers)
+def test_include(parser):
+    command_line._router(
+        'yamale/tests/command_line_fixtures/yamls/good.yaml',
+        'schema_needs_include.yaml', 1, parser, include='yamale/tests/command_line_fixtures/yamalevalidators.py')
+
 
 def test_empty_schema_file():
     with pytest.raises(ValueError, match='is an empty file!'):
