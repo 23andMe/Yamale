94b43e0fbd95b180f4de8bfb8bdeed1c34a30361
Oleg Moroz <o.moroz@1440.space>
2022-12-13 11:16:46 +0300

    Use utf-8 as default yaml file encoding

--- yamale/readers/yaml_reader.py
+++ yamale/readers/yaml_reader.py
@@ -31,7 +31,7 @@ def parse_yaml(path=None, parser='pyyaml', content=None):
     if (path is None and content is None) or (path is not None and content is not None):
         raise TypeError("Pass either path= or content=, not both")
     if path is not None:
-        with open(path) as f:
+        with open(path, encoding='utf8') as f:
             return parse(f)
     else:
         return parse(StringIO(content))
