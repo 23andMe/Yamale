e13f6f5301eaa827aed910b4608306fef4c97bb0
kiblik <kiblik@gjh.sk>
2024-04-30 21:54:03 +0200

    Fix regex for Mac

--- yamale/validators/validators.py
+++ yamale/validators/validators.py
@@ -243,8 +243,8 @@ class Mac(Regex):
     def __init__(self, *args, **kwargs):
         super(Mac, self).__init__(*args, **kwargs)
         self.regexes = [
-            re.compile(r"[0-9a-fA-F]{2}([-:]?)[0-9a-fA-F]{2}(\\1[0-9a-fA-F]{2}){4}$"),
-            re.compile(r"[0-9a-fA-F]{4}([-:]?)[0-9a-fA-F]{4}(\\1[0-9a-fA-F]{4})$"),
+            re.compile(r"[0-9a-fA-F]{2}([-:]?)[0-9a-fA-F]{2}(\1[0-9a-fA-F]{2}){4}$"),
+            re.compile(r"[0-9a-fA-F]{4}([-:]?)[0-9a-fA-F]{4}(\1[0-9a-fA-F]{4})$"),
         ]
 
 
